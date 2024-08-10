ec16e6b541035f14c74f2dcc47260492f0dd58e0
saeed-shiri <saeed66.sh@gmail.com>
2024-08-10 20:11:11 +0330

    add docker compose file

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,38 +1,46 @@
 using Catalog.Infrastructure;
 using Catalog.Application;
 using Catalog.API;
-using Catalog.Infrastructure.Data;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using HealthChecks.UI.Client;
 
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services
-    .AddApiServices(builder.Configuration)
-    .AddApplicationServices(builder.Configuration)
-    .AddInfrastructureServices(builder.Configuration);
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+class Program
 {
-    app.UseDeveloperExceptionPage();
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services
+            .AddApiServices(builder.Configuration)
+            .AddApplicationServices(builder.Configuration)
+            .AddInfrastructureServices(builder.Configuration);
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseHealthChecks("/health", new HealthCheckOptions()
+        {
+            Predicate = _ => true,
+            ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse,
+        });
+
+        app.MapControllers();
+
+        app.Run();
+    }
 }
 
-app.UseHttpsRedirection();
 
-app.UseHealthChecks("/health", new HealthCheckOptions()
-{
-    Predicate = _ => true,
-    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse,   
-});
 
-app.MapControllers();
-
-app.Run();
