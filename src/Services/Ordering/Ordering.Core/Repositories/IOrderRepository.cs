35cd718b74b5ff33e5ae3839b467dcb67911de12
saeed-shiri <saeed66.sh@gmail.com>
2024-08-31 18:47:33 +0330

    add ordering project

---
+++ src/Services/Ordering/Ordering.Core/Repositories/IOrderRepository.cs
@@ -0,0 +1,9 @@
+﻿
+
+using Ordering.Core.Entities;
+
+namespace Ordering.Core.Repositories;
+public interface IOrderRepository : IBaseRepository<Order>
+{
+    Task<IReadOnlyList<Order>> GetAllOrdersByUsername(string username);
+}
